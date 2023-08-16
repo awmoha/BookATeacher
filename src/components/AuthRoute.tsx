6fd3000990a20df56dca8c178b0e20f72d510540
mvdgragt <michiel@vandergragt.eu>
2023-08-16 17:07:27 +0200

    Added Firebas

---
+++ src/components/AuthRoute.tsx
@@ -0,0 +1,40 @@
+import {FunctionComponent, useEffect, useState, ReactNode} from "react";
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { useNavigate } from "react-router-dom";
+export interface IAuthRouteProps {children:ReactNode;
+};
+
+const AuthRoute: FunctionComponent<IAuthRouteProps> = props => {
+    const { children } = props;
+    const auth = getAuth();
+    const navigate = useNavigate();
+    const [loading,setLoading] = useState(false);
+
+    useEffect(() => {
+        AuthCheck();
+        return () => {AuthCheck()}
+    },[auth]);
+
+    const AuthCheck = onAuthStateChanged(auth, (user) => {
+        if (user) {
+            setLoading(false)
+        } else {
+            console.log('unautorized');
+            navigate('/login')
+        }
+        
+    });
+
+    if (loading) return <p>
+        loading...
+    </p>
+
+
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
+
+export default AuthRoute;
